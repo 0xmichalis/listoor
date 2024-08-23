9359f3cba49ec386be61a3d7ece239f1d8ea8fa1
Michalis Kargakis <kargakis@protonmail.com>
2024-08-24 00:46:39 +0200

    Script updates

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+CONTAINER_NAME="listoor"
+
+podman stop ${CONTAINER_NAME} > /dev/null 2>&1
+podman rm -f ${CONTAINER_NAME} > /dev/null 2>&1
+
+echo "Starting ${CONTAINER_NAME}..."
 podman run -d \
-  --name listoor \
+  --name ${CONTAINER_NAME} \
   --env-file .env \
   -v $(pwd)/data/collections.json:/app/data/collections.json \
   listoor:latest
